7a9010d26abb50d68c5e19206f5bc6e507b0bce8
Pecheche <davideguerrerop@gmail.com>
2020-02-18 13:46:00 -0500

    Commit

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -22,10 +22,6 @@
 
     @csrf
 
-    <input name="name" placeholder="Nombre..."> <br>
-    <input type="email" name="email" placeholder="Email..."> <br>
-    <input name="subject" placeholder="Asunto..."> <br>
-    <textarea name="content" placeholder="Mensaje..."></textarea> <br>
     <button>Enviar</button>
 
 
